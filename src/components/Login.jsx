47404b4f4f4889f6d3bd8c6d3c5d7a5d98f03b87
rabiyarumi <rabiyaarumi@gmail.com>
2024-11-12 14:09:40 +0600

    added login page

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,44 +1,110 @@
+import { sendPasswordResetEmail, signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../firebase.init";
+import { useRef, useState } from "react";
+import { Link } from "react-router-dom";
+
 const Login = () => {
+  const [success, setSuccess] = useState(false);
+  const [error, setError] = useState("");
+  const emailRef = useRef();
+
+  const handleLogin = (event) => {
+    event.preventDefault();
+    const email = event.target.email.value;
+    const password = event.target.password.value;
+    console.log(email, password);
+
+    //manage state
+    setSuccess(false);
+    setError("");
+
+    //login users
+    signInWithEmailAndPassword(auth, email, password)
+      .then((result) => {
+        console.log(result.user);
+        
+        if(!result.user.emailVerified){
+          setError("Please verify your email address.")
+          return;
+        }
+        else{
+          setSuccess(true);
+          
+        }
+      })
+      .catch((error) => {
+        console.log(error.message);
+        setSuccess(false);
+        setError(error.message);
+      });
+  };
+
+  const handleForgetPassword = () => {
+    console.log('email:', emailRef.current.value);
+    const email = emailRef.current.value;
+
+    if(!email){
+      console.log("provide valid email")
+      return;
+    }
+    else{
+      sendPasswordResetEmail(auth, email)
+      .then(() => {
+        alert("Password Reset email sent, Please check your email")
+      })
+    }
+  }
   return (
     <div className="text-center">
       <h2 className="text-3xl font-bold text-sky-400  border-sky-400">Login</h2>
 
-      <form>
-        <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl mx-auto my-10">
-          <div className="card-body">
-            <div className="form-control">
-              <label className="label">
-                <span className="label-text">Email</span>
-              </label>
-              <input
-                type="email"
-                placeholder="email"
-                className="input input-bordered"
-                required
-              />
-            </div>
-            <div className="form-control">
-              <label className="label">
-                <span className="label-text">Password</span>
-              </label>
-              <input
-                type="password"
-                placeholder="password"
-                className="input input-bordered"
-                required
-              />
-              <label className="label">
-                <a href="#" className="label-text-alt link link-hover">
-                  Forgot password?
-                </a>
-              </label>
-            </div>
-            <div className="form-control mt-6">
-              <button className="btn bg-sky-400">Login</button>
-            </div>
+      <form onSubmit={handleLogin}>
+        <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl mx-auto my-10 p-6">
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Email</span>
+            </label>
+            <input
+              type="email"
+              name="email"
+              ref={emailRef}
+              placeholder="email"
+              className="input input-bordered"
+              required
+            />
+          </div>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Password</span>
+            </label>
+            <input
+              type="password"
+              name="password"
+              placeholder="password"
+              className="input input-bordered"
+              required
+            />
+            <label className="label">
+              <a onClick={handleForgetPassword}  href="#" className="label-text-alt link link-hover">
+                Forgot password?
+              </a>
+            </label>
+          </div>
+          <div className="form-control mt-6">
+            <button className="btn bg-sky-400 text-white font-bold text-lg">
+              Login
+            </button>
           </div>
         </div>
       </form>
+      <div>
+        {success && (
+          <p className="text-green-600 mt-2">User login successfully</p>
+        )}
+        {error && <p className="text-red-600">{error}</p>}
+      </div>
+      <p> Don't have an account? Please <Link to={'/signup'} className="text-sky-400 mb-4 hover:underline">Sign Up</Link> </p>
+      
     </div>
   );
 };
--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,95 +1,132 @@
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword, sendEmailVerification } from "firebase/auth";
 import { auth } from "../firebase.init";
 import { useState } from "react";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 const SignUp = () => {
-  const [successMessage, setSuccessMessage] = useState(false)
+  const [successMessage, setSuccessMessage] = useState(false);
   const [errorMessage, setErrorMessage] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
 
-    const handleSignUp = (event) => {
-        event.preventDefault();
-        const email = event.target.email.value;
-        const password = event.target.password.value;
-        console.log(email, password);
+  const handleSignUp = (event) => {
+    event.preventDefault();
+    const email = event.target.email.value;
+    const password = event.target.password.value;
+    const terms = event.target.terms.checked;
+    console.log(email, password, terms);
 
-        //reset error message
-        setErrorMessage("");
-        setSuccessMessage(false);
+    //reset error message
+    setErrorMessage("");
+    setSuccessMessage(false);
 
-        //check password validation
-        const passwordRegex = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]{6,}$/;
+    //check password validation
+    const passwordRegex =
+      /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]{6,}$/;
 
-        if(!passwordRegex.test(password)){
-          setErrorMessage("Password must be at least 6 characters long, contain at least one uppercase letter, one lowercase letter, one number, and one special character."
-        );
-        return;
-        }
-        if(password.length < 6) {
-          setErrorMessage("Password must be 6 characters or longer")
-          return;
-        }
-
-
-
-        //creat authentication from firebase
-        createUserWithEmailAndPassword(auth, email, password)
-        .then((result) => {
-          console.log(result.user);
-          setSuccessMessage(true)
-        })
-        .catch((error) => {
-          console.log(error.message)
-          console.log(error.code)
-          setErrorMessage(error.code)
-          setSuccessMessage(false)
-        })
-
+    if (!passwordRegex.test(password)) {
+      setErrorMessage(
+        "Password must be at least 6 characters long, contain at least one uppercase letter, one lowercase letter, one number, and one special character."
+      );
+      return;
     }
+    if (password.length < 6) {
+      setErrorMessage("Password must be 6 characters or longer");
+      return;
+    }
+
+    //check terms & condition
+    if(!terms){
+      setErrorMessage("Please accept our terms & condition")
+      return;
+    }
+
+    //create authentication from firebase
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((result) => {
+        console.log(result.user);
+        setSuccessMessage(true);
+
+        //email verification
+        sendEmailVerification(auth.currentUser)
+        .then(() => {
+          console.log("Email verified")
+        })
+      })
+      .catch((error) => {
+        console.log(error.message);
+        console.log(error.code);
+        setErrorMessage(error.code);
+        setSuccessMessage(false);
+      });
+  };
   return (
     <div className="text-center">
       <h2 className="text-3xl font-bold text-sky-400  border-sky-400 my-10">
         Sign Up
       </h2>
-      <form onSubmit={handleSignUp} className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl mx-auto p-6">
+      <form
+        onSubmit={handleSignUp}
+        className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl mx-auto p-6"
+      >
+        <label className="label">
+          <span className="label-text">Email</span>
+        </label>
+        <input
+          type="email"
+          name="email"
+          placeholder="email"
+          className="input input-bordered"
+          required
+        />
+
+        <div className="relative">
+          <label className="label">
+            <span className="label-text">Password</span>
+          </label>
+          <input
+            type={showPassword ? "text" : "password"}
+            name="password"
+            placeholder="password"
+            className="input input-bordered w-full"
+            required
+          />
+
+          <button
+            onClick={() => setShowPassword(!showPassword)}
+            className="btn btn-xs absolute right-3 bottom-3"
+          >
+            {showPassword ? <FaEyeSlash></FaEyeSlash> : <FaEye></FaEye>}
+          </button>
+        </div>
+        
+
+        <div className="form-control my-3 ">
+          <label className="cursor-pointer label justify-start gap-4">
+          <input
+              type="checkbox"
+              name="terms"
+              className="checkbox checkbox-info"
+            />
+            <span className="label-text">Accept Out Terms & Condition</span>
            
-              <label className="label">
-                <span className="label-text">Email</span>
-              </label>
-              <input
-                type="email"
-                name="email"
-                placeholder="email"
-                className="input input-bordered"
-                required
-              />
-      
-          
-              <label className="label">
-                <span className="label-text">Password</span>
-              </label>
-              <input
-                type="password"
-                name="password"
-                placeholder="password"
-                className="input input-bordered"
-                required
-              />
-              <label className="label">
-                <a href="#" className="label-text-alt link link-hover">
-                  Forgot password?
-                </a>
-              </label>
-           
-            <div className="form-control mt-6">
-              <button className="btn bg-sky-400 text-white font-bold text-lg">Sign Up</button>
+          </label>
+        </div>
+
+        <div className="form-control ">
+          <button className="btn bg-sky-400 text-white font-bold text-lg">
+            Sign Up
+          </button>
         </div>
       </form>
-      {
-        errorMessage && <p className="text-red-600 my-3">Error: {errorMessage}</p>
-      }
-      {
-        successMessage && <p className="text-green-600">Successfully Singed Up!</p>
-      }
+      {errorMessage && (
+        <p className="text-red-600 my-3">Error: {errorMessage}</p>
+      )}
+      {successMessage && (
+        <p className="text-green-600">Successfully Singed Up!</p> 
+      )}
+            <p>Already have an account? Please <Link to={'/login'} className=" text-sky-400 hover:underline">Login</Link> </p>
+
     </div>
   );
 };
